f1292a5230fbcac7947ff3a82ab46450e8606d81
Deverneton9938 <55164394+Deverneton9938@users.noreply.github.com>
2023-02-11 11:55:26 -0600

    Autonomis Square code (Cameron)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -147,40 +147,50 @@ public class Robot extends TimedRobot {
   //function that is called periodically during autonomous
   @Override
   public void autonomousPeriodic() {
-    for(int i = 0; i < 4; i+=1) {
-      
+    for(int i = -1; i < 4; i += 2) {
+      if (Timer.getFPGATimestamp() - autoStart > i) {
+        driveLeftA.set(0.1);
+        driveLeftB.set(0.1);
+        driveRightA.set(0.1);
+        driveRightB.set(0.1);
+      }
+      else if (Timer.getFPGATimestamp() - autoStart > i + 1) {
+        driveLeftA.set(-0.4);
+        driveLeftB.set(-0.4);
+        driveRightA.set(0.4);
+        driveRightB.set(0.4);
+      } 
+      else {
+        driveLeftA.set(0);
+        driveLeftB.set(0);
+        driveRightA.set(0);
+        driveRightB.set(0);
+      }
     }
-    if (Timer.getFPGATimestamp() - autoStart > 3) {
+  }
+    
+    /** else if (Timer.getFPGATimestamp() - autoStart > 3){
       driveLeftA.set(0.1);
       driveLeftB.set(0.1);
       driveRightA.set(0.1);
       driveRightB.set(0.1);
-    }
-    else if (Timer.getFPGATimestamp() - autoStart > 3) {
-      driveLeftA.set(-0.4);
-      driveLeftB.set(-0.4);
-      driveRightA.set(0.4);
-      driveRightB.set(0.4);
-    }
-      else if (Timer.getFPGATimestamp() - autoStart > 3){
-      driveLeftA.set(0.1);
-      driveLeftB.set(0.1);
-      driveRightA.set(0.1);
-      driveRightB.set(0.1);
-    }
-    else if (Timer.getFPGATimestamp() - autoStart > 3) {
+    } 
+    else if (Timer.getFPGATimestamp() - autoStart > 4) {
       driveLeftA.set(-0.4);
       driveLeftB.set(-0.4);
       driveRightA.set(0.4);
       driveRightB.set(0.4);
     } 
-    else {
-      driveLeftA.set(0);
-      driveLeftB.set(0);
-      driveRightA.set(0);
-      driveRightB.set(0);
+    else if (Timer.getFPGATimestamp() - autoStart > 5) {
+      driveLeftA.set(0.1);
+      driveLeftB.set(0.1);
+      driveRightA.set(0.1);
+      driveRightB.set(0.1);
     }
-  }
+    else if (Timer.getFPGATimestamp() - autoStart > 6) {
+      driveLeftA.set()
+    } */
+    
 
   /** This function is called once when teleop is enabled. */
   @Override
